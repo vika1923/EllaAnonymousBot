7113060da64e664533086700e14580dd42befae7
Viktoriya <75164029+vika1923@users.noreply.github.com>
2024-05-28 01:19:44 +0500

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 
 Token = '5357565720:AAE9D7rX6NGol-Jjh041Ypx61NWgvanBM-0'
-specialists_chat_id = -1002083540330
+specialists_chat_id = 1    # INSERT VALID CHAT ID
 
 bot = Bot(token=Token)
 dp = Dispatcher()
